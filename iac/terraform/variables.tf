a2d94d4cdcfafcb28c44f9bc0c0f5aac308b9d7e
CountGoncho <richardhealy@microsoft.com>
2023-04-15 19:00:59 +0100

    new content 2

--- iac/terraform/variables.tf
+++ iac/terraform/variables.tf
@@ -1,6 +1,13 @@
 variable "location" {
   description = ""
   type        = string
+  default = "uk south"
+}
+
+variable "resource_group" {
+  description = ""
+  type        = string
+  default     = "Marketing"
 }
 
 variable "uniquer" {
@@ -13,4 +20,33 @@ variable "resources_prefix" {
   description = ""
   type        = string
   default     = null
+}
+
+variable "storage_account_name" {
+  description = "The name of the storage account"
+  type = string   
+}
+variable "storage_account_tier" {
+  description = "Storage Account Tier"
+  type = string   
+}
+variable "storage_account_replication_type" {
+  description = "Storage Account Replication Type"
+  type = string   
+}
+variable "storage_account_kind" {
+  description = "Storage Account Kind"
+  type = string   
+}
+variable "static_website_index_document" {
+  description = "static website index document"
+  type = string   
+}
+variable "static_website_error_404_document" {
+  description = "static website error 404 document"
+  type = string   
+}
+variable "static_website_source_folder" {
+  description = "static website source folder"
+  type = string  
 }
