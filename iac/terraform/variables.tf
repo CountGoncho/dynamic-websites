f3919b4e209c084869b48a4420bbbeaa0820fc67
CountGoncho <richardhealy@microsoft.com>
2023-04-15 12:18:09 +0100

    commit main files

---
+++ iac/terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "location" {
+  description = ""
+  type        = string
+}
+
+variable "uniquer" {
+  description = ""
+  type        = string
+  default     = null
+}
+
+variable "resources_prefix" {
+  description = ""
+  type        = string
+  default     = null
+}
