3a72906145cd61441251e4811a50db7b47a29b2a
CountGoncho <richardhealy@microsoft.com>
2023-04-16 13:53:14 +0100

    updated variables 7 with merge

--- iac/terraform/variables.tf
+++ iac/terraform/variables.tf
@@ -30,7 +30,7 @@ variable "storage_account_name" {
 
 variable "storage_account_tier" {
   description = "Storage Account Tier"
-  type        = string
+  type        = string   
   default     = "Standard"
 }
 
@@ -38,4 +38,28 @@ variable "storage_account_replication_type" {
   description = "Storage Account Replication Type"
   type        = string
   default     = "LRS"   
+}
+
+variable "storage_account_kind" {
+  description = "Storage Account Kind"
+  type        = string
+  default     = "StorageV2"   
+}
+
+variable "static_website_index_document" {
+  description = "static website index document"
+  type        = string   
+  default     = "index.html"
+}
+
+variable "static_website_error_404_document" {
+  description = "static website error 404 document"
+  type        = string   
+  default     = "error.html"
+}
+
+variable "static_website_source_folder" {
+  description = "static website source folder"
+  type        = string  
+  default     = "web"
 }
