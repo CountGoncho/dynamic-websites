c3626da138fc1ff4b6ff1d1a5668d1c084749da4
CountGoncho <richardhealy@microsoft.com>
2023-04-16 13:55:13 +0100

    updated variables 8 formatting

--- iac/terraform/variables.tf
+++ iac/terraform/variables.tf
@@ -30,36 +30,36 @@ variable "storage_account_name" {
 
 variable "storage_account_tier" {
   description = "Storage Account Tier"
-  type        = string   
+  type        = string
   default     = "Standard"
 }
 
 variable "storage_account_replication_type" {
   description = "Storage Account Replication Type"
   type        = string
-  default     = "LRS"   
+  default     = "LRS"
 }
 
 variable "storage_account_kind" {
   description = "Storage Account Kind"
   type        = string
-  default     = "StorageV2"   
+  default     = "StorageV2"
 }
 
 variable "static_website_index_document" {
   description = "static website index document"
-  type        = string   
+  type        = string
   default     = "index.html"
 }
 
 variable "static_website_error_404_document" {
   description = "static website error 404 document"
-  type        = string   
+  type        = string
   default     = "error.html"
 }
 
 variable "static_website_source_folder" {
   description = "static website source folder"
-  type        = string  
+  type        = string
   default     = "web"
 }
