c09321431e11638b18991a949b3d3b525fd7c2b1
CountGoncho <richardhealy@microsoft.com>
2023-04-16 18:36:21 +0100

    updated variables 13

--- iac/terraform/main.tf
+++ iac/terraform/main.tf
@@ -1,6 +1,6 @@
 # Create Azure Storage account
 resource "azurerm_storage_account" "storage_account" {
-  name                = "${local.resources_prefix}sa"
+  name                = "marketingtestsa"
   resource_group_name = local.resource_group_name
 
   location                 = var.location
