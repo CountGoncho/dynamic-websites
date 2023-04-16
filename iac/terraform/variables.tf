8c2e9e268bf1635d5dcc31fc0a37f682e98afd43
CountGoncho <52013387+CountGoncho@users.noreply.github.com>
2023-04-16 13:08:33 +0100

    Update variables.tf

--- iac/terraform/variables.tf
+++ iac/terraform/variables.tf
@@ -33,3 +33,32 @@ variable "storage_account_tier" {
   type        = string
   default     = "Standard"
 }
+
+variable "storage_account_replication_type" {
+  description = "Storage Account Replication Type"
+  type        = string
+  default     = "LRS"   
+}
+
+variable "storage_account_kind" {
+  description = "Storage Account Kind"
+  type        = string
+  default     = "StorageV2"   
+}
+variable "static_website_index_document" {
+  description = "static website index document"
+  type        = string   
+  default     = "index.html"
+}
+
+variable "static_website_error_404_document" {
+  description = "static website error 404 document"
+  type        = string   
+  default     = "error.html"
+}
+
+variable "static_website_source_folder" {
+  description = "static website source folder"
+  type        = string  
+  default     = "web"
+}
