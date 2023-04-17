edb955887c92a2c3da62154b31357503fb78653f
CountGoncho <richardhealy@microsoft.com>
2023-04-17 20:47:01 +0100

    updated variables plus new static

---
+++ iac/terraform/modules/storage/variables.tf
@@ -0,0 +1,11 @@
+variable "resource_unique_id" {
+  description = "(required) resource unique identifier"
+  type        = string
+  default     = null
+}
+
+variable "resource_all_prefix" {
+  description = "prefix for resources"
+  type        = string
+  default     = null
+}
