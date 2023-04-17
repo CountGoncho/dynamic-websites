491063fe2dee1670184cd96e0d7d4929016d3cb8
CountGoncho <richardhealy@microsoft.com>
2023-04-17 20:51:29 +0100

    updated variables plus new static

--- iac/terraform/modules/storage/variables.tf
+++ iac/terraform/modules/storage/variables.tf
@@ -9,3 +9,69 @@ variable "resource_all_prefix" {
   type        = string
   default     = null
 }
+
+variable "location" {
+  description = ""
+  type        = string
+  default     = "uk south"
+}
+
+variable "resource_group" {
+  description = ""
+  type        = string
+  default     = "marketingweb"
+}
+
+variable "uniquer" {
+  description = ""
+  type        = string
+  default     = null
+}
+
+variable "resources_prefix" {
+  description = ""
+  type        = string
+  default     = null
+}
+
+variable "storage_account_list" {
+  description = "The base names of the storage account"
+  type        = list(any)
+  default     = ["marketing3de4k", "marketingg35tgw", "marketing3kdj2"]
+}
+
+variable "storage_account_tier" {
+  description = "Storage Account Tier"
+  type        = string
+  default     = "Standard"
+}
+
+variable "storage_account_replication_type" {
+  description = "Storage Account Replication Type"
+  type        = string
+  default     = "LRS"
+}
+
+variable "storage_account_kind" {
+  description = "Storage Account Kind"
+  type        = string
+  default     = "StorageV2"
+}
+
+variable "static_website_index_document" {
+  description = "static website index document"
+  type        = string
+  default     = "index.html"
+}
+
+variable "static_website_error_404_document" {
+  description = "static website error 404 document"
+  type        = string
+  default     = "error.html"
+}
+
+variable "static_website_source_folder" {
+  description = "static website source folder"
+  type        = string
+  default     = "web"
+}
