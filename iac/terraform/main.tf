d71f289cbe4bb2cb38eb3c40f98775ff705b6a52
CountGoncho <richardhealy@microsoft.com>
2023-04-17 16:22:54 +0100

    updated variables

--- iac/terraform/main.tf
+++ iac/terraform/main.tf
@@ -17,7 +17,7 @@ resource "azurerm_storage_account" "storage_account" {
 resource "azurerm_storage_blob" "webload" {
   for_each = fileset(path.module, "public/**")
 
-  name                   = each.key
+  name                   = trimprefix(each.key, "public/")
   storage_account_name   = azurerm_storage_account.storage_account.name
   storage_container_name = local.storage_container_name
   type                   = "Block"
